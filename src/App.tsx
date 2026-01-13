3c0be5c1fc50b219a769adf58e76ee3b24b212e8
MysunKim <gongo0207@gmail.com>
2026-01-14 00:25:19 +0900

    기본레이아웃구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import { Layout } from './components/Layout'
+import { TodoList } from './pages/todoList'
 
 export function App() {
 
   return (
     <Layout>
-      <h1>Hello React + TypeScript</h1>
+      <TodoList/>
     </Layout>
   )
 }
